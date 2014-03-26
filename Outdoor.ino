3b152f90c4e80f8d0684e945ccb5322a0fc426ae
faribamustafa <faribamustafa@users.noreply.github.com>
2014-03-26 15:16:54 +0600

    Update Outdoor.ino

--- Outdoor.ino
+++ Outdoor.ino
@@ -6,16 +6,18 @@
 
  © ⒻⒶⓇⒾⒷⒶ ⓂⓊⓈⓉⒶⒻⒶ
  
+
+  
 #include <Servo.h>
 
 Servo watergate; // create korlam servo object
 
 int servopin = 11; 
  
- int lowest = analogRead(A0);
+ 
 void setup() {
 
-  
+  //Serial.begin(9600);
  watergate.attach(servopin);
  
   watergate.write(180);
@@ -33,15 +35,27 @@ void loop() {
   
 //  Serial.println(present);
  
-  if (present <= lowest){
+  if (present > 0 && present <= 100 ){
     
   watergate.write(0);
   delay(1000);
   
   }
-  else{ 
+  
+  if ( present > 100 && present <= 250){
+    watergate.write(45);
+    delay(1000);
+  }
+  
+ if ( present > 250 && present <= 800){
+    watergate.write(90);
+    delay(1000);
+  }
+if ( present > 800 ){
     watergate.write(180);
     delay(1000);
   }
-  delay(500);        
+ 
+  
+  delay(300);        // delay in between reads for stability
 }
